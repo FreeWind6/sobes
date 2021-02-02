b7720df4eb3e6346f39057270758943a869fc8ea
Dmitry <free-wind-2012@yandex.ru>
2021-02-02 21:52:17 +0300

    Add hw 2

---
+++ src/main/java/SecondLesson/LinkedList.java
@@ -0,0 +1,71 @@
+package SecondLesson;
+
+/**
+ * @author Dmitry Kubyshkin
+ * @dateOfCreation 02.02.2021
+ */
+
+public class LinkedList<T> {
+
+    private Node first;
+    private int size;
+
+    public LinkedList() {
+        first = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(T data) {
+        Node current = first;
+
+        if (current == null) {
+            first = new Node(data);
+            size++;
+            return;
+        }
+
+        while (current.getNext() != null) {
+            current = current.getNext();
+        }
+
+        current.setNext(new Node(data));
+        size++;
+    }
+
+    public void add(T[] array) {
+        for (T data : array) {
+            add(data);
+        }
+    }
+
+    private class Node<T> {
+
+        private T data;
+        private Node next;
+
+        public Node(T data) {
+            this.data = data;
+            next = null;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+    }
+}
