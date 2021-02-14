60b0501f98dc537e0f94b1b16798d89859829a00
Dmitry <free-wind-2012@yandex.ru>
2021-02-14 13:29:19 +0300

    Add hw 5

---
+++ fiveLesson/src/main/java/com/example/demo/repositories/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repositories;
+
+
+import com.example.demo.entities.database.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+
+}
